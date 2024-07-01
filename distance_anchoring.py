832e6b123631bc396cc3154a43b4600f53d604ba
SebSchroe <seb.schroe1@gmail.com>
2024-07-01 17:16:22 +0200

    new data saving structure

--- distance_anchoring.py
+++ distance_anchoring.py
@@ -11,7 +11,7 @@ import pandas as pd
 
 # global variables
 DIR = pathlib.Path(os.curdir)
-normalization_method = None
+normalization_method = "mgb_normalization"
 port = "COM5"
 slider = serial.Serial(port, baudrate=9600, timeout=0, rtscts=False)
 speaker_dict = {0: 2.00,
@@ -26,6 +26,11 @@ speaker_dict = {0: 2.00,
                 9: 11.00,
                 10: 11.80} # TODO: adjust values to current setup
 
+# TODO: add function to test speaker
+# TODO: adjust code to new logging-thing
+# TODO: add variable for condition
+# TODO: smooth out training() and test() with more functions
+
 #def get_precomputed_USOs (ms):
 #    USO_file_folder = DIR / 'data' / 'cutted_USOs' / f'uso_{ms}ms'
  #   USO_file_names = os.listdir(DIR / 'data' / 'cutted_USOs' / f'uso_{ms}ms')
@@ -37,22 +42,15 @@ USO_file_names = os.listdir(DIR / 'data' / 'cutted_USOs' / 'uso_300ms')
 precomputed_USOs = slab.Precomputed([slab.Sound(os.path.join(USO_file_folder, f)) for f in USO_file_names])
 
 # initialize setup to connect to the processors
-def initialize_setup(normalization_algorithm="rms", normalization_sound_type="syllable"): # TODO: attributes can be removed
-    global normalization_method
+def initialize_setup():
     procs = ["RX81", "RX8", DIR / "data" / "rcx" / "cathedral_play_buf.rcx"]
     freefield.initialize("cathedral", device=procs, zbus=False, connection="USB")
     freefield.SETUP = "cathedral"
     freefield.SPEAKERS = freefield.read_speaker_table()
-    '''
-    normalization_file = DIR / "data" / "calibration" / f"calibration_cathedral_{normalization_sound_type}_{normalization_algorithm}.pkl" # has to be adjusted
-    freefield.load_equalization(file=pathlib.Path(normalization_file), frequency=False)
-    normalization_method = f"{normalization_sound_type}_{normalization_algorithm}"
-    '''
-    normalization_method = "mgb_normalization"
-    freefield.set_logger("DEBUG")
+    freefield.set_logger("DEBUG") #TODO: Lukas is using 'INFO' now.
 
 # main code for running the experiments training phase
-def training(sub_id, block_id, task_id, n_reps=45, isi=2):
+def training(cond_id, sub_id, block_id, task_id, n_reps=45, isi=2):
 
     # set condition for this block
     if task_id == 1:
@@ -65,7 +63,7 @@ def training(sub_id, block_id, task_id, n_reps=45, isi=2):
     # initialize sequence with corresponding conditions
     seq = slab.Trialsequence(conditions=1, n_reps=n_reps)
     for trial in seq:
-        
+
         # get random USO sound
         random_index = random.randint(0, len(precomputed_USOs) - 1)
         USO = slab.Sound(precomputed_USOs[random_index])
@@ -74,29 +72,29 @@ def training(sub_id, block_id, task_id, n_reps=45, isi=2):
         # read slider value and convert it to speaker value
         slider_value = get_slider_value()
         closest_speaker = min(speaker_dic, key=lambda k: abs(speaker_dic[k] - slider_value)) # calculates speaker which is closest to distance of the slider value
-       
+
         # play USO from speaker corresponding to to slider value
         USO = apply_mgb_equalization(signal=USO, speaker=freefield.pick_speakers(closest_speaker)[0])
         freefield.set_signal_and_speaker(signal=USO, speaker=closest_speaker, equalize=False)
         freefield.play(kind=1, proc='RX81')
 
         # finish this trial
-        event_id = seq.this_n + 1 # TODO: event_id = seq.this_n? start counting at 0 or 1?
-        print('Trial:', event_id)
-        print('Slider value', slider_value)
-        print('Closest speaker', closest_speaker)
+        event_id = seq.this_n + 1
+        print(f'Trial: {event_id}')
+        print(f'Slider value: {slider_value}')
+        print(f'Closest speaker: {closest_speaker}')
         # freefield.flush_buffers(processor='RX81')
-        freefield.write(tag='data0', value=0, processors='RX81') # TODO: is it enough or do I have to send all other buffer to 99
+        freefield.write(tag='data0', value=0, processors='RX81') # TODO: is it enough or do I have to send all other buffer to 99 or do I dont have to flush the buffer. Only fush buffer after experiment
         time.sleep(isi)
-        
+
         # save results
-        save_results(event_id=event_id, sub_id=sub_id, block_id=block_id, task_id=task_id,
-                     stim_id=stim_id, speaker_id=closest_speaker, response_time=0,
-                     response=slider_value, n_reps=n_reps, isi=isi)
+        save_results(cond_id=cond_id, sub_id=sub_id, block_id=block_id, task_id=task_id,
+                     event_id=event_id, stim_id=stim_id, speaker_id=closest_speaker, response=slider_value,
+                     response_time=np.nan, n_reps=n_reps, isi=isi)
     print("Done with training")
 
 # main code for running the experiments test phase
-def test(sub_id, block_id, task_id, n_reps, isi=2):
+def test(cond_id, sub_id, block_id, task_id, n_reps, isi=0): # TODO: think about isi
 
     # set condition for this block
     if task_id == 1: # n_reps = 5 for 55 trials
@@ -106,9 +104,8 @@ def test(sub_id, block_id, task_id, n_reps, isi=2):
         nearest_speaker = 1
         farthest_speaker = 6
     elif task_id == 3:# n_reps = 5 for 55 trials
-        nearest_speaker = 0
-        farthest_speaker = 10
-        frequency = 500 # TODO: think about third condition (maybe filter would be best)
+        nearest_speaker = 4
+        farthest_speaker = 9
     else:
         return print('You can only set task_id to 1, 2 or 3')
 
@@ -116,12 +113,12 @@ def test(sub_id, block_id, task_id, n_reps, isi=2):
     speakers = list(range(nearest_speaker, farthest_speaker + 1))
     seq = slab.Trialsequence(conditions=speakers, n_reps=n_reps)
     for speaker in seq:
-        
+
         # get random USO sound
         random_index = random.randint(0, len(precomputed_USOs) - 1)
         USO = slab.Sound(precomputed_USOs[random_index])
         stim_id = USO_file_names[random_index]
-        
+
         # prepare and playing USO
         USO = apply_mgb_equalization(signal=USO, speaker=freefield.pick_speakers(speaker)[0])
         freefield.set_signal_and_speaker(signal=USO, speaker=speaker, equalize=False)
@@ -129,52 +126,54 @@ def test(sub_id, block_id, task_id, n_reps, isi=2):
 
         # wait for response and read it
         time_before = time.time()
-        response = get_slider_value()
+        slider_value = get_slider_value()
         time_after = time.time()
 
         # finish this trial
         response_time = time_after - time_before
-        event_id = seq.this_n + 1 # Q: event_id = seq.this_n? start counting at 0 or 1?
+        event_id = seq.this_n + 1
         print('Trial:', event_id)
         print('speaker_id', speaker)
         print('response', response)
         # freefield.flush_buffers(processor='RX81')
         freefield.write(tag='data0', value=0, processors='RX81') # clear buffer
-        time.sleep(isi) #TODO: think about isi
+        time.sleep(isi)
 
         # save data event by event
-        save_results(event_id=event_id, sub_id=sub_id, block_id=block_id, task_id=task_id,
-                     stim_id=stim_id, speaker_id=speaker, response=response,
-                     response_time=response_time, n_reps=n_reps, isi=isi)
+        save_results(cond_id=cond_id, sub_id=sub_id, block_id=block_id, task_id=task_id,
+                     event_id=event_id, stim_id=stim_id, speaker_id=speaker, response=slider_value, response_time=response_time,
+                     n_reps=n_reps, isi=isi)
     print("Done with test")
 
 
-def save_results(event_id, sub_id, block_id, task_id, stim_id, speaker_id, response, response_time, n_reps, isi): # TODO: think about datastructure
+def save_results(cond_id, sub_id, block_id, task_id, event_id, stim_id, speaker_id, response, response_time, n_reps, isi):
 
     # create file name
-    file_name = DIR / 'results' / f'results_sub-{sub_id}_block-{block_id}_task-{task_id}.csv'
+    file_name = DIR / 'results' / f'results_cond-{cond_id}_sub-{sub_id}_block-{block_id}_task-{task_id}.csv'
     if file_name.exists():
         df_curr_results = pd.read_csv(file_name)
     else:
         df_curr_results = pd.DataFrame()
 
     # convert values in desired data types
-    event_id = int(event_id)
+    cond_id = int(cond_id)
     sub_id = int(sub_id)
     block_id = int(block_id)
-    task_id = int(task_id) # condition 1, 2 or 3
+    task_id = int(task_id)
+    event_id = int(event_id)
     stim_id = str(stim_id)
     speaker_id = int(speaker_id)
     response = float(response)
     response_time = float(response_time)
-    n_reps = float(n_reps)
+    n_reps = int(n_reps)
     isi = float(isi)
 
     # building current data structure
-    new_row = {'event_id' : event_id,
+    new_row = {'cond_id' : cond_id,
         'sub_id' : sub_id,
         'block_id' : block_id,
         'task_id' : task_id,
+        'event_id' : event_id,
         'stim_id' : stim_id,
         'speaker_id' : speaker_id,
         'response' : response,
@@ -182,7 +181,7 @@ def save_results(event_id, sub_id, block_id, task_id, stim_id, speaker_id, respo
         'n_reps' : n_reps,
         'isi' : isi}
 
-    # add row to df
+    # add row to df and add df to csv file
     df_curr_results = df_curr_results._append(new_row, ignore_index=True)
     df_curr_results.to_csv(file_name, mode='w', header=True, index=False)
 
@@ -225,3 +224,19 @@ def apply_mgb_equalization(signal, speaker, mgb_loudness=30, fluc=0):
 def get_speaker_normalization_level(speaker):
     a, b, c = get_log_parameters(speaker.distance)
     return logarithmic_func(x=30, a=a, b=b, c=c)
+
+def set_ids(cond_id, block_id):
+    if cond_id == 1:
+        if block_id == 6:
+            task_id = 3
+        elif block_id == 4:
+            task_id = 2
+        else:
+            task_id = 1
+        return block_id, task_id
+    if cond_id == 2:
+        if block_id == 6:
+            task_id = 1
+        else:
+            task_id = 2
+    return block_id, task_id
--- distance_anchoring_experiment.py
+++ distance_anchoring_experiment.py
@@ -1,24 +1,36 @@
 # run this code line by line and only set limited variables
-
+import freefield
 import distance_anchoring
-sub_id = -2
+
+sub_id = 0
+cond_id = 1
 
 """
+condition:  1 -> task_id = 1 for block 1, 2, 3, 5,
+                 task_id = 2 for block 4,
+                 task_id = 3 for block 6
+            2 -> task_id = 1 for block 6,
+                 task_id = 2 for block 1, 2, 3, 4, 5
+
 task_id:    1 -> nearest_speaker = 0, farthest_speaker = 10
             2 -> nearest_speaker = 1, farthest_speaker = 6
-            3 -> nearest_speaker = 0, farthest_speaker = 10 + manipulation of reverb
+            3 -> nearest_speaker = 4, farthest_speaker = 9
 """
-
+# initialize setup
 distance_anchoring.initialize_setup()
 
-# block 1: visual limited test
-distance_anchoring.test(sub_id=sub_id, block_id=1, task_id=2, n_reps=2, isi=0.5)
+# block 1: test visual limited
+block_id, task_id = set_ids(cond_id=cond_id, block_id=1)
+distance_anchoring.test(cond_id=cond_id, sub_id=sub_id, block_id=block_id, task_id=task_id, n_reps=2, isi=0)
+freefield.flush_buffers(processor='RX81')
 
-# block 2: visual unlimited test
-# distance_anchoring.test(sub_id=sub_id, block_id=2, task_id=1, n_reps=1, play_via='cathedral')
+# block 2: test visual
+# block_id, task_id = set_ids(cond_id=cond_id, block_id=2)
+# distance_anchoring.test(cond_id=cond_id, sub_id=sub_id, block_id=block_id, task_id=task_id, n_reps=2, isi=0)
 
-# block 3: visual unlimited training
-# distance_anchoring.training(sub_id=sub_id, block_id=3, task_id=2, n_reps=15, isi=2)
+# block 3: training visual
+block_id, task_id = set_ids(cond_id=cond_id, block_id=3)
+distance_anchoring.training(cond_id=cond_id, sub_id=sub_id, block_id=block_id, task_id=task_id, n_reps=15, isi=2)
 
 # block 4: visual unlimited test
 # distance_anchoring.test()
