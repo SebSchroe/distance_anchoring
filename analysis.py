04b40260d6d40add8cb799d12b67058b0867f407
SebSchroe <seb.schroe1@gmail.com>
2024-07-04 16:10:59 +0200

    first data analysis

--- analysis.py
+++ analysis.py
@@ -3,9 +3,10 @@ import os
 import pandas as pd
 import matplotlib.pyplot as plt
 
-sub_id = -2
-block_id = 1
-task_id = 1
+sub_id = 1
+cond_id = 2
+block_id = 5
+task_id = 2
 
 speaker_dict = {0: 2.00,
                 1: 3.00,
@@ -17,10 +18,10 @@ speaker_dict = {0: 2.00,
                 7: 9.00,
                 8: 10.00,
                 9: 11.00,
-                10: 11.80}
+                10: 12.00}
 
 DIR = pathlib.Path(os.curdir)
-file_path = DIR / 'results' / f'results_sub-{sub_id}_block-{block_id}_task-{task_id}.csv'
+file_path = DIR / 'results' / f'results_cond-{cond_id}_sub-{sub_id}_block-{block_id}_task-{task_id}.csv'
 df = pd.read_csv(file_path)
 
 # convert speaker_id to real distance
@@ -31,12 +32,12 @@ df['speaker_distance'] = df['speaker_id'].apply(lambda x: get_value_from_dict(x,
 
 # create figure
 plt.figure(figsize=(8, 8))
-plt.plot(df['speaker_distance'], df['response'], marker='o', linestyle='None')
+plt.plot(df['event_id'], df['response'], marker='o', linestyle='None')
 
-plt.title(f'sub = {sub_id}, block = {block_id}, task = {task_id}')
+plt.title(f'sub = {sub_id}, cond = {cond_id}, block = {block_id}, task = {task_id}')
 plt.xlabel('presented distance [m]')
-plt.ylabel('perceived distance [m]')
-plt.axis([0, 13, 0, 13])
+plt.ylabel('slider value [m]')
+plt.axis([0, 100, 0, 13])
 plt.gca().set_aspect('equal', adjustable='box')
 
 plt.show()
