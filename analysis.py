b1d9fdfbb59485c4a942afd932ac33aa453a8d3d
SebSchroe <seb.schroe1@gmail.com>
2024-07-08 15:10:56 +0200

    further improvements

--- analysis.py
+++ analysis.py
@@ -3,6 +3,7 @@ import os
 import pandas as pd
 import matplotlib.pyplot as plt
 from sklearn.linear_model import LinearRegression
+from sklearn.metrics import r2_score
 
 sub_id = 1
 cond_id = 1
@@ -22,7 +23,7 @@ speaker_dict = {0: 2.00,
                 10: 12.00}
 
 DIR = pathlib.Path(os.curdir)
-file_path = DIR / 'results' / f'results_cond-{cond_id}_sub-{sub_id}_block-{block_id}_task-{task_id}.csv'
+file_path = DIR / 'results' / f'results_sub-{sub_id}_cond-{cond_id}_block-{block_id}_task-{task_id}.csv'
 df = pd.read_csv(file_path)
 
 # data manipulation
@@ -34,29 +35,39 @@ df['speaker_distance'] = df['speaker_id'].apply(lambda x: get_value_from_dict(x,
 
 # data analysis
 def linear_regression(x_values, y_values):
+    
+    # linear regression
     x  = df[f'{x_values}'].values.reshape(-1, 1)
     y = df[f'{y_values}'].values
     model = LinearRegression()
     model.fit(x, y)
     y_pred = model.predict(x)
     
+    # calculate regression coefficients
+    slope = model.coef_[0]
+    intercept = model.intercept_
+    r_squared = r2_score(y, y_pred)
+    textstr = f'Slope: {slope:.2f}\nIntercept: {intercept:.2f}\nR²: {r_squared:.2f}'
+    
+    # plotting
     plt.figure(figsize=(8, 8))
     plt.scatter(x, y, color='blue', label='Data')
     plt.plot(x, y_pred, color='red', linewidth=1.5, label='Linear regression')
+    plt.text(0.03, 0.97, textstr, transform=plt.gca().transAxes,
+             verticalalignment='top', bbox=dict(boxstyle='round,pad=0.5', facecolor='white', alpha=0.5))
     
+    # appearence
     plt.xlabel(f'{x_values}')
     plt.ylabel(f'{y_values}')
-    plt.title(f'Regression of sub {sub_id}, cond {cond_id}, block {block_id}')
-    plt.axis([0, 12, 0 ,12])
+    plt.title(f'sub {sub_id}, cond {cond_id}, block {block_id}')
+    plt.axis([0, 13, 0 ,13])
     plt.gca().set_aspect('equal', adjustable='box')
-    plt.legend(loc='upper left')
+    plt.legend(loc='lower right')
     
     plt.show()
-    print(f'Slope: {model.coef_[0]}')
-    print(f'R^2: {model.score(x, y)}')
     
     
-linear_regression('speaker_id', 'response')
+linear_regression('speaker_distance', 'response')
     
 
 # create figure
