805d131c82c5b499ed81b7faf30442c1c856ad0b
SebSchroe <seb.schroe1@gmail.com>
2024-07-16 14:10:16 +0200

    more analysis

--- analysis.py
+++ analysis.py
@@ -20,152 +20,105 @@ speaker_dict = {0: 2.00,
                 10: 12.00}
 
 # data analysis
-def plot_tests_split(sub_id, cond_id, x_values, y_values):
-    
-    # prepare multi plots
-    fig, axes = plt.subplots(1, 3, figsize=(19, 4))
-    index = 0
-    
-    # build each subplot per block
-    for block in [1, 4, 6]:
-        
-        # load data
-        block_id = block
-        task_id = get_task_id(cond_id=cond_id, block_id=block_id)
-        file_path = DIR / 'results' / f'results_sub-{sub_id}_cond-{cond_id}_block-{block_id}_task-{task_id}.csv'
-        df = pd.read_csv(file_path)
-        
-        # manipulate data
-        df['speaker_distance'] = df['speaker_id'].apply(lambda x: get_value_from_dict(x, speaker_dict))
-        df['response_speaker_id_1_6'] = np.where((df['speaker_id'] >= 1) & (df['speaker_id'] <= 6), df['response'], np.nan)
-        df['response_speaker_id_4_9'] = np.where((df['speaker_id'] >= 4) & (df['speaker_id'] <= 9), df['response'], np.nan)
-        
-        # linear regression
-        if cond_id == 1 and block == 1:
-            x = df['speaker_distance'].values.reshape(-1, 1)
-            y_1 = df['response_speaker_id_1_6']
-            y_2 = df['response_speaker_id_4_9']
-            model_1 = LinearRegression()
-            model_2 = LinearRegression()
-            model_1.fit(x, y_1)
-            model_2.fit(x, y_2)
-            y_1_pred = model_1.predict(x)
-            y_2_pred = model_2.predict(x)
-        else:    
-            x  = df[f'{x_values}'].values.reshape(-1, 1)
-            y = df[f'{y_values}'].values
-            model = LinearRegression()
-            model.fit(x, y)
-            y_pred = model.predict(x)
-        
-        # calculate regression coefficients
-        slope = model.coef_[0]
-        intercept = model.intercept_
-        r_squared = r2_score(y, y_pred)
-        textstr = f'Slope: {slope:.2f}\nIntercept: {intercept:.2f}\nR²: {r_squared:.2f}'
+def get_response_subset_df(df, nearest_speaker, farthest_speaker):
+    df['response_subset'] = np.where((df['speaker_id'] >= nearest_speaker) & (df['speaker_id'] <= farthest_speaker), df['response'], np.nan)
+    response_subset = df[['speaker_id', 'speaker_distance', 'response_subset']].copy()
+    response_subset.dropna(subset='response_subset', inplace=True)
+    return response_subset
 
-        # plotting
-        axes[index].scatter(x, y, color='blue', alpha=0.25, edgecolors='None', label='Data points')
-        axes[index].plot(x, y_pred, color='blue', linewidth=1.5, label='Linear regression')
-        axes[index].plot([0, 13], [0, 13], color='grey', linewidth=1.5, linestyle='--', label='Optimum')
-        axes[index].text(0.03, 0.97, textstr, transform=axes[index].transAxes,
-                         verticalalignment='top', bbox=dict(boxstyle='round,pad=0.5', facecolor='white', alpha=0.5))
-        
-        # layout
-        axes[index].set_xlabel(f'{x_values}')
-        axes[index].set_ylabel(f'{y_values}')
-        axes[index].set_title(f'sub: {sub_id}, cond: {cond_id}, block: {block_id}')
-        axes[index].set_xlim(0, 13)
-        axes[index].set_ylim(0, 13)
-        axes[index].set_aspect('equal', adjustable='box')
-        #axes[index].legend(loc='lower right')
-        index += 1
-    
-    plt.tight_layout()
-    plt.show() 
-
-def split_block_1(sub_id, cond_id):
-    
-    # load data
-    block_id = 1
+def get_df(sub_id, cond_id, block_id):
     task_id = get_task_id(cond_id=cond_id, block_id=block_id)
     file_path = DIR / 'results' / f'results_sub-{sub_id}_cond-{cond_id}_block-{block_id}_task-{task_id}.csv'
     df = pd.read_csv(file_path)
+    return df
+
+def get_linear_regression_values(df, x_values, y_values):
     
-    # manipulate data
+    # calculate linear regression
+    x = df[f'{x_values}'].values.reshape(-1, 1)
+    y = df[f'{y_values}']
+    model = LinearRegression()
+    model.fit(x, y)
+    y_pred = model.predict(x)
+    
+    # calculate linear regression coefficients
+    slope = model.coef_[0]
+    intercept = model.intercept_
+    r_squared = r2_score(y, y_pred)
+    regression_coefficients = f'Slope: {slope:.2f}\nIntercept: {intercept:.2f}\nR²: {r_squared:.2f}'
+    return x, y, y_pred, regression_coefficients
+
+def split_data(sub_id, cond_id):
+    
+    # load data depending on condition
+    if cond_id == 1:
+        block_id = 1
+    if cond_id == 2:
+        block_id = 6
+    df = get_df(sub_id, cond_id, block_id)
+    
+    # transform speaker_id to real speaker distance
     df['speaker_distance'] = df['speaker_id'].apply(lambda x: get_value_from_dict(x, speaker_dict))
     
-    df['response_speaker_id_1_6'] = np.where((df['speaker_id'] >= 1) & (df['speaker_id'] <= 6), df['response'], np.nan)
-    df_1 = df[['speaker_id', 'speaker_distance', 'response_speaker_id_1_6']].copy()
-    df_1.dropna(subset='response_speaker_id_1_6', inplace=True)
-
-    df['response_speaker_id_4_9'] = np.where((df['speaker_id'] >= 4) & (df['speaker_id'] <= 9), df['response'], np.nan)
-    df_2 = df[['speaker_id', 'speaker_distance', 'response_speaker_id_4_9']].copy()
-    df_2.dropna(subset='response_speaker_id_4_9', inplace=True)
+    # create df subsets of certain speaker range
+    df_1 = get_response_subset_df(df, 1, 6)
+    df_2 = get_response_subset_df(df, 4, 9)
     
-    # linear regression
-    x_1 = df_1['speaker_distance'].values.reshape(-1, 1)
-    y_1 = df_1['response_speaker_id_1_6']
+    # do linear regression and get necessary values + coefficients
+    x_1, y_1, y_1_pred, regression_coefficients_1 = get_linear_regression_values(df_1, 'speaker_distance', 'response_subset')
+    x_2, y_2, y_2_pred, regression_coefficients_2 = get_linear_regression_values(df_2, 'speaker_distance', 'response_subset')
     
-    x_2 = df_2['speaker_distance'].values.reshape(-1, 1)
-    y_2 = df_2['response_speaker_id_4_9']
-    
-    model_1 = LinearRegression()
-    model_1.fit(x_1, y_1)
-    y_1_pred = model_1.predict(x_1)
-    
-    model_2 = LinearRegression()
-    model_2.fit(x_2, y_2)
-    y_2_pred = model_2.predict(x_2)
     
     # plotting
     plt.scatter(x_1, y_1, color='blue', alpha=0.25, edgecolors='None', label='Data points')
     plt.plot(x_1, y_1_pred, color='blue', linewidth=1.5, label='Linear regression')
+    plt.text(0.03, 0.97, regression_coefficients_1, transform=plt.gca().transAxes,
+             verticalalignment='top', bbox=dict(boxstyle='round,pad=0.5', facecolor='white', alpha=0.5))
     
     plt.scatter(x_2, y_2, color='red', alpha=0.25, edgecolors='None', label='Data points')
     plt.plot(x_2, y_2_pred, color='red', linewidth=1.5, label='Linear regression')
+    plt.text(0.97, 0.03, regression_coefficients_2, transform=plt.gca().transAxes,
+             verticalalignment='top', bbox=dict(boxstyle='round,pad=0.5', facecolor='white', alpha=0.5))
     
     plt.plot([0, 13], [0, 13], color='grey', linewidth=1.5, linestyle='--', label='Optimum')
     
+    # layout
+    plt.xlabel('speaker_distance')
+    plt.ylabel('response')
+    plt.title(f'sub: {sub_id}, cond: {cond_id}, block: {block_id}')
+    plt.xlim(0, 13)
+    plt.ylim(0, 13)
+    plt.gca().set_aspect('equal', adjustable='box')
+    
     plt.show()
+    
+    return x_1, y_1, y_1_pred, x_2, y_2, y_2_pred
 
 
-def plot_tests(sub_id, cond_id, x_values, y_values):
+def plot_test_blocks(sub_id, cond_id, x_values, y_values):
     
     # prepare multi plots
     fig, axes = plt.subplots(1, 3, figsize=(19, 4))
     index = 0
     
     # build each subplot per block
-    for block in [1, 4, 6]:
+    for block_id in [1, 4, 6]:
         
         # load data
-        block_id = block
-        task_id = get_task_id(cond_id=cond_id, block_id=block_id)
-        file_path = DIR / 'results' / f'results_sub-{sub_id}_cond-{cond_id}_block-{block_id}_task-{task_id}.csv'
-        df = pd.read_csv(file_path)
+        block_id = block_id
+        df = get_df(sub_id, cond_id, block_id)
         
-        # manipulate data
+        # transform speaker_id to corresponding speaker distance
         df['speaker_distance'] = df['speaker_id'].apply(lambda x: get_value_from_dict(x, speaker_dict))
         
-        # linear regression
-        x  = df[f'{x_values}'].values.reshape(-1, 1)
-        y = df[f'{y_values}'].values
-        model = LinearRegression()
-        model.fit(x, y)
-        y_pred = model.predict(x)
-        
-        # calculate regression coefficients
-        slope = model.coef_[0]
-        intercept = model.intercept_
-        r_squared = r2_score(y, y_pred)
-        textstr = f'Slope: {slope:.2f}\nIntercept: {intercept:.2f}\nR²: {r_squared:.2f}'
+        # do linear regression and get necessary values + coefficients
+        x, y, y_pred, regression_coefficients = get_linear_regression_values(df, f'{x_values}', f'{y_values}')
 
         # plotting
         axes[index].scatter(x, y, color='blue', alpha=0.25, edgecolors='None', label='Data points')
         axes[index].plot(x, y_pred, color='blue', linewidth=1.5, label='Linear regression')
         axes[index].plot([0, 13], [0, 13], color='grey', linewidth=1.5, linestyle='--', label='Optimum')
-        axes[index].text(0.03, 0.97, textstr, transform=axes[index].transAxes,
+        axes[index].text(0.03, 0.97, regression_coefficients, transform=axes[index].transAxes,
                          verticalalignment='top', bbox=dict(boxstyle='round,pad=0.5', facecolor='white', alpha=0.5))
         
         # layout
--- run_analysis.py
+++ run_analysis.py
@@ -2,13 +2,13 @@
 import analysis
 
 # global variables
-sub_id = 4
-cond_id = 1
+sub_id = 1
+cond_id = 2
 
 x_values = 'speaker_distance'
 y_values = 'response'
 
 # multi plot
-#analysis.plot_tests(sub_id=sub_id, cond_id=cond_id, x_values=x_values, y_values=y_values)
+# analysis.plot_test_blocks(sub_id=sub_id, cond_id=cond_id, x_values=x_values, y_values=y_values)
 
-analysis.split_block_1(sub_id=sub_id, cond_id=cond_id)
+analysis.split_data(sub_id=sub_id, cond_id=cond_id)
