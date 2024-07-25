5c2d417e613d2e236e729f28e64c230fca89a269
SebSchroe <seb.schroe1@gmail.com>
2024-07-25 11:21:56 +0200

    more analysis and statistics

--- analysis.py
+++ analysis.py
@@ -24,6 +24,8 @@ def get_response_subset_df(df, nearest_speaker, farthest_speaker):
     df['response_subset'] = np.where((df['speaker_id'] >= nearest_speaker) & (df['speaker_id'] <= farthest_speaker), df['response'], np.nan)
     response_subset = df[['speaker_id', 'speaker_distance', 'response_subset']].copy()
     response_subset.dropna(subset='response_subset', inplace=True)
+    response_subset.rename(columns={'response_subset': 'response'}, inplace=True)
+    
     return response_subset
 
 def get_df(sub_id, cond_id, block_id):
@@ -95,7 +97,7 @@ def split_data(sub_id, cond_id):
     return x_1, y_1, y_1_pred, x_2, y_2, y_2_pred
 
 
-def plot_test_blocks(sub_id, cond_id, x_values, y_values):
+def plot_presented_vs_percieved_distance(sub_id, cond_id, x_values, y_values, split=False):
     
     # prepare multi plots
     fig, axes = plt.subplots(1, 3, figsize=(19, 4))
@@ -105,30 +107,64 @@ def plot_test_blocks(sub_id, cond_id, x_values, y_values):
     for block_id in [1, 4, 6]:
         
         # load data
-        block_id = block_id
         df = get_df(sub_id, cond_id, block_id)
         
         # transform speaker_id to corresponding speaker distance
         df['speaker_distance'] = df['speaker_id'].apply(lambda x: get_value_from_dict(x, speaker_dict))
-        
-        # do linear regression and get necessary values + coefficients
-        x, y, y_pred, regression_coefficients = get_linear_regression_values(df, f'{x_values}', f'{y_values}')
 
-        # plotting
-        axes[index].scatter(x, y, color='blue', alpha=0.25, edgecolors='None', label='Data points')
-        axes[index].plot(x, y_pred, color='blue', linewidth=1.5, label='Linear regression')
-        axes[index].plot([0, 13], [0, 13], color='grey', linewidth=1.5, linestyle='--', label='Optimum')
-        axes[index].text(0.03, 0.97, regression_coefficients, transform=axes[index].transAxes,
-                         verticalalignment='top', bbox=dict(boxstyle='round,pad=0.5', facecolor='white', alpha=0.5))
+        # split data of block 1 or 6 (depending on cond) in desired response subsets
+        if split and ((cond_id == 1 and block_id == 1) or (cond_id == 2 and block_id == 6)):
+               
+            # get new dataframes for subset
+            df_1 = get_response_subset_df(df, 1, 6)
+            df_2 = get_response_subset_df(df, 4, 9)
+            
+            # do linear regression for each subset
+            x_1, y_1, y_pred_1, regression_coefficients_1 = get_linear_regression_values(df_1, f'{x_values}', f'{y_values}')
+            x_2, y_2, y_pred_2, regression_coefficients_2 = get_linear_regression_values(df_2, f'{x_values}', f'{y_values}')
+
+            # plot each subset
+            axes[index].plot([0, 13], [0, 13], color='grey', linewidth=1.5, linestyle='--', label='Optimum')
+            
+            axes[index].scatter(x_1, y_1, color='blue', alpha=0.25, edgecolors='None', label='Data points')
+            axes[index].plot(x_1, y_pred_1, color='blue', linewidth=1.5, label='Linear regression')
+            axes[index].text(0.03, 0.97, regression_coefficients_1, transform=axes[index].transAxes,
+                             verticalalignment='top', horizontalalignment='left', bbox=dict(boxstyle='round,pad=0.5', facecolor='white', alpha=0.5))
+            
+            axes[index].scatter(x_2, y_2, color='red', alpha=0.25, edgecolors='None', label='Data points')
+            axes[index].plot(x_2, y_pred_2, color='red', linewidth=1.5, label='Linear regression')
+            axes[index].text(0.97, 0.03, regression_coefficients_2, transform=axes[index].transAxes,
+                             verticalalignment='bottom', horizontalalignment='right', bbox=dict(boxstyle='round,pad=0.5', facecolor='white', alpha=0.5))
+            
+            # layout
+            axes[index].set_xlabel(f'{x_values}')
+            axes[index].set_ylabel(f'{y_values}')
+            axes[index].set_title(f'sub: {sub_id}, cond: {cond_id}, block: {block_id}')
+            axes[index].set_xlim(0, 13)
+            axes[index].set_ylim(0, 13)
+            axes[index].set_aspect('equal', adjustable='box')
+         
+        # if split is false show default datapoints
+        else:
+            
+            # do linear regression and get necessary values + coefficients
+            x, y, y_pred, regression_coefficients = get_linear_regression_values(df, f'{x_values}', f'{y_values}')
+            
+            # plotting
+            axes[index].scatter(x, y, color='blue', alpha=0.25, edgecolors='None', label='Data points')
+            axes[index].plot(x, y_pred, color='blue', linewidth=1.5, label='Linear regression')
+            axes[index].plot([0, 13], [0, 13], color='grey', linewidth=1.5, linestyle='--', label='Optimum')
+            axes[index].text(0.03, 0.97, regression_coefficients, transform=axes[index].transAxes,
+                             verticalalignment='top', bbox=dict(boxstyle='round,pad=0.5', facecolor='white', alpha=0.5))
+            
+            # layout
+            axes[index].set_xlabel(f'{x_values}')
+            axes[index].set_ylabel(f'{y_values}')
+            axes[index].set_title(f'sub: {sub_id}, cond: {cond_id}, block: {block_id}')
+            axes[index].set_xlim(0, 13)
+            axes[index].set_ylim(0, 13)
+            axes[index].set_aspect('equal', adjustable='box')
         
-        # layout
-        axes[index].set_xlabel(f'{x_values}')
-        axes[index].set_ylabel(f'{y_values}')
-        axes[index].set_title(f'sub: {sub_id}, cond: {cond_id}, block: {block_id}')
-        axes[index].set_xlim(0, 13)
-        axes[index].set_ylim(0, 13)
-        axes[index].set_aspect('equal', adjustable='box')
-        #axes[index].legend(loc='lower right')
         index += 1
     
     plt.tight_layout()
--- run_analysis.py
+++ run_analysis.py
@@ -8,7 +8,5 @@ cond_id = 2
 x_values = 'speaker_distance'
 y_values = 'response'
 
-# multi plot
-# analysis.plot_test_blocks(sub_id=sub_id, cond_id=cond_id, x_values=x_values, y_values=y_values)
-
-analysis.split_data(sub_id=sub_id, cond_id=cond_id)
+# plot presented vs percieved distance of block 1, 4 and 6
+analysis.plot_presented_vs_percieved_distance(sub_id=sub_id, cond_id=cond_id, x_values=x_values, y_values=y_values, split=True)
