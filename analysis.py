d074ecd2859ad3713ef845b6852958c3f4e929da
SebSchroe <seb.schroe1@gmail.com>
2024-09-18 15:16:57 +0200

    new analysis

--- analysis.py
+++ analysis.py
@@ -23,14 +23,14 @@ speaker_dict = {0: 2.00,
                 8: 10.00,
                 9: 11.00,
                 10: 12.00}
-cond_1_sub_ids = [1, 3, 4]
-cond_2_sub_ids = [1, 2]
+cond_1_sub_ids = [1, 3, 4, 7]
+cond_2_sub_ids = [1, 2, 6]
 
 # analysation of statistical power (predict sample size)
 def predict_sample_size(effect_size, alpha=0.05, power=0.8, alternative='two-sided'):
     '''
     effect_size = Cohen's d
-    # alternative can be two-sided', 'larger' or 'smaller'
+    # alternative = two-sided', 'larger' or 'smaller'
     '''
     
     analysis = smp.TTestIndPower()
@@ -49,10 +49,10 @@ def create_diagnostic_plots(sub_id, cond_id, block_id):
     
     # load data
     if sub_id == 'all':
-        df = get_concat_df(cond_id, block_id)
+        df, sub_ids = get_concat_df(cond_id, block_id)
     else:
         df = get_df(sub_id, cond_id, block_id)
-    
+        
     # transform data
     df['speaker_distance'] = df['speaker_id'].apply(lambda x: get_speaker_distance(x, speaker_dict))
     
@@ -65,8 +65,37 @@ def create_diagnostic_plots(sub_id, cond_id, block_id):
     vif, fig, ax = cls()
     print(vif)
 
+def plot_data(cond_id, block_ids):
+    
+    # load data and concatenate all data
+    
+    
+    # prepare multiplot
+    n_plots = len(block_ids)
+    fig, axes = plt.subplots(1, n_plots, figsize=(19, 4), squeeze=False)
+    index = 0
+    
+    for block_id in block_ids:
+    
+        # load data
+        df, sub_ids = get_concat_df(cond_id, block_id)
+        
+        # transform speaker_id to corresponding speaker distance
+        df['speaker_distance'] = df['speaker_id'].apply(lambda x: get_speaker_distance(x, speaker_dict))
+        
+        # get means per participant
+        df_means = df.groupby(['sub_id', 'speaker_distance'], as_index=False).agg(
+            mean_response=('response', 'mean'))
+        
+        sns.lineplot(data=df_means, x='speaker_distance', y='mean_response', hue='sub_id', ax=axes[0, index], palette='tab10')
+        
+        index += 1
+    
+    plt.tight_layout()    
+    plt.show()
+
 # plot presented vs. percieved distances of test blocks 1, 4 and 6
-def plot_presented_vs_percieved_distance(sub_id, cond_id, split=False):
+def plot_presented_vs_percieved_distance(sub_id, cond_id, block_ids, split=False):
     '''
     sub_id: can be individual sub_id or 'all' for all data of certain condition
     split: splits data of first block (condition 1) or last block (condition 2) in speaker subsets 1-6 and 4-9
@@ -77,11 +106,12 @@ def plot_presented_vs_percieved_distance(sub_id, cond_id, split=False):
     y_values = 'response'
     
     # prepare multi plots
-    fig, axes = plt.subplots(1, 3, figsize=(19, 4))
+    n_plots = len(block_ids)
+    fig, axes = plt.subplots(1, n_plots, figsize=(19, 4))
     index = 0
     
     # build each subplot per block
-    for block_id in [1, 4, 6]:
+    for block_id in block_ids:
         
         # load data
         if sub_id == 'all':
@@ -93,7 +123,7 @@ def plot_presented_vs_percieved_distance(sub_id, cond_id, split=False):
         df['speaker_distance'] = df['speaker_id'].apply(lambda x: get_speaker_distance(x, speaker_dict))
 
         # split data of block 1 or 6 (depending on cond) in desired response subsets
-        if split and ((cond_id == 1 and block_id == 1) or (cond_id == 2 and block_id == 6)):
+        if split and ((cond_id == 1 and block_id == 2) or (cond_id == 2 and block_id == 6)):
                
             # get new dataframes for subset
             df_1 = get_response_subset_df(df, 1, 6)
@@ -104,26 +134,28 @@ def plot_presented_vs_percieved_distance(sub_id, cond_id, split=False):
             x_2, y_2, y_pred_2, regression_coefficients_2 = get_linear_regression_values(df_2, f'{x_values}', f'{y_values}')
 
             # plot baseline
-            axes[index].plot([0, 13], [0, 13], color='grey', linewidth=1.5, linestyle='--', label='Optimum')
+            axes[index].plot([2, 12], [2, 12], color='grey', linewidth=1.5, linestyle='--', label='Optimum')
             
             # plot subset 1
             axes[index].scatter(x_1, y_1, color='blue', alpha=0.05, edgecolors='None', label='Data points')
             axes[index].plot(x_1, y_pred_1, color='blue', linewidth=1.5, label='Linear regression')
             axes[index].text(0.03, 0.97, regression_coefficients_1, transform=axes[index].transAxes,
-                             verticalalignment='top', horizontalalignment='left', bbox=dict(boxstyle='round,pad=0.5', facecolor='white', alpha=0.5))
+                             verticalalignment='top', horizontalalignment='left', bbox=dict(boxstyle='round,pad=0.5', facecolor='white', edgecolor='none', alpha=0.5))
             
             # plot subset 2
             axes[index].scatter(x_2, y_2, color='red', alpha=0.05, edgecolors='None', label='Data points')
             axes[index].plot(x_2, y_pred_2, color='red', linewidth=1.5, label='Linear regression')
             axes[index].text(0.97, 0.03, regression_coefficients_2, transform=axes[index].transAxes,
-                             verticalalignment='bottom', horizontalalignment='right', bbox=dict(boxstyle='round,pad=0.5', facecolor='white', alpha=0.5))
+                             verticalalignment='bottom', horizontalalignment='right', bbox=dict(boxstyle='round,pad=0.5', facecolor='white', edgecolor='none', alpha=0.5))
             
             # layout
-            axes[index].set_xlabel('presented distance [m]')
-            axes[index].set_ylabel('percieved distance [m]')
+            axes[index].set_xlabel('Source distance [m]')
+            axes[index].set_ylabel('Perceived distance [m]')
             axes[index].set_title(f'sub: {sub_id}, cond: {cond_id}, block: {block_id}')
             axes[index].set_xlim(0, 13)
             axes[index].set_ylim(0, 13)
+            axes[index].set_xticks(range(0, 13, 2))
+            axes[index].set_yticks(range(0, 13, 2))
             axes[index].set_aspect('equal', adjustable='box')
          
         # if split is false show default datapoints
@@ -135,16 +167,18 @@ def plot_presented_vs_percieved_distance(sub_id, cond_id, split=False):
             # plotting
             axes[index].scatter(x, y, color='blue', alpha=0.05, edgecolors='None', label='Data points')
             axes[index].plot(x, y_pred, color='blue', linewidth=1.5, label='Linear regression')
-            axes[index].plot([0, 13], [0, 13], color='grey', linewidth=1.5, linestyle='--', label='Optimum')
+            axes[index].plot([2, 12], [2, 12], color='grey', linewidth=1.5, linestyle='--', label='Optimum')
             axes[index].text(0.03, 0.97, regression_coefficients, transform=axes[index].transAxes,
-                             verticalalignment='top', bbox=dict(boxstyle='round,pad=0.5', facecolor='white', alpha=0.5))
+                             verticalalignment='top', bbox=dict(boxstyle='round,pad=0.5', facecolor='white', edgecolor='none', alpha=0.5))
             
             # layout
-            axes[index].set_xlabel('presented distance [m]')
-            axes[index].set_ylabel('percieved distance [m]')
+            axes[index].set_xlabel('Source distance [m]')
+            axes[index].set_ylabel('Perceived distance [m]')
             axes[index].set_title(f'sub: {sub_id}, cond: {cond_id}, block: {block_id}')
             axes[index].set_xlim(0, 13)
             axes[index].set_ylim(0, 13)
+            axes[index].set_xticks(range(0, 13, 2))
+            axes[index].set_yticks(range(0, 13, 2))
             axes[index].set_aspect('equal', adjustable='box')
         
         index += 1
@@ -155,6 +189,52 @@ def plot_presented_vs_percieved_distance(sub_id, cond_id, split=False):
     print('Total n:', len(sub_ids))
     print('Total trials:', len(df))
     return df
+
+def plot_means(sub_id, cond_id, block_ids):
+    
+    # prepare multiplot
+    n_plots = len(block_ids)
+    fig, axes = plt.subplots(1, n_plots, figsize=(19, 4))
+    index = 0
+    
+    for block_id in block_ids:
+        # get data
+        if sub_id == 'all':
+            df, sub_ids = get_concat_df(cond_id, block_id)
+        else:
+            df = get_df(sub_id, cond_id, block_id)
+        
+        # transform speaker_id to corresponding speaker distance
+        df['speaker_distance'] = df['speaker_id'].apply(lambda x: get_speaker_distance(x, speaker_dict))
+        
+        # get new df with means at each speaker position
+        means_df = get_means_df(df)
+        
+        # extraxt values from df
+        x = means_df['speaker_distance']
+        means = means_df['mean_response']
+        std = means_df['std_response']
+        
+        # plotting
+        axes[index].errorbar(x, means, yerr=std, fmt='o', capsize=5, color='blue', ecolor='blue', label='mean with std')
+        axes[index].plot([2, 12], [2, 12], color='grey', linewidth=1.5, linestyle='--', label='Optimum')
+        
+        #layout
+        axes[index].set_xlabel('Source distance [m]')
+        axes[index].set_ylabel('Averaged perceived distance [m]')
+        axes[index].set_title(f'sub: {sub_id}, cond: {cond_id}, block: {block_id}')
+        axes[index].set_xlim(0, 13)
+        axes[index].set_ylim(0, 13)
+        axes[index].set_xticks(range(0, 13, 2))
+        axes[index].set_yticks(range(0, 13, 2))
+        axes[index].set_aspect('equal', adjustable='box')
+        
+        index += 1
+    
+    plt.tight_layout()
+    plt.show()
+    
+    return means_df
 
 # plot average slopes and standard deviation
 def seperate_slope_model():
@@ -199,7 +279,7 @@ def seperate_slope_model():
 def plot_signed_error_distribution_at_x(cond_id, block_id, x=2):
     
     # load data
-    df = get_concat_df(cond_id, block_id)
+    df, sub_ids = get_concat_df(cond_id, block_id)
     
     # transform speaker_id to corresponding speaker distance
     df['speaker_distance'] = df['speaker_id'].apply(lambda x: get_speaker_distance(x, speaker_dict))
@@ -209,8 +289,8 @@ def plot_signed_error_distribution_at_x(cond_id, block_id, x=2):
     df_filtered = df[df['speaker_distance'] == x]
     
     # plotting
-    plt.figure(figsize=(10, 6))
-    sns.histplot(df_filtered['signed_error'], kde=True, bins=20, color='blue')
+    plt.figure(figsize=(15, 4))
+    sns.histplot(df_filtered['signed_error'], kde=True, binwidth=0.1, color='blue')
     
     # layout
     plt.title(f'signed error distribution at presented distance = {x}, block {block_id}')
@@ -259,23 +339,28 @@ def get_df(sub_id, cond_id, block_id):
     df = pd.read_csv(file_path)
     return df
 
-def get_concat_df(cond_id, block_id):
+def get_concat_df(cond_id, block_ids):
     
-    # create empty dataframe and get task id
+    # create empty dataframe
     concat_df = pd.DataFrame()
-    task_id = get_task_id(cond_id, block_id)
     
     # get sub_ids depending on cond_id
     if cond_id == 1:
         sub_ids = cond_1_sub_ids
     else:
         sub_ids = cond_2_sub_ids
+        
+    # loop through all block_ids
+    for block_id in block_ids:
+        # get task_id of current block
+        task_id = get_task_id(cond_id, block_id)
+        
+        # concatenate data for all subjects for the current block_id 
+        for sub_id in sub_ids:
+            file_path = DIR / 'results' / f'results_sub-{sub_id}_cond-{cond_id}_block-{block_id}_task-{task_id}.csv'
+            new_df = pd.read_csv(file_path)
+            concat_df = pd.concat([concat_df, new_df], axis=0, ignore_index=True)
     
-    # concatenate all dataframes into one merged df 
-    for sub_id in sub_ids:
-        file_path = DIR / 'results' / f'results_sub-{sub_id}_cond-{cond_id}_block-{block_id}_task-{task_id}.csv'
-        new_df = pd.read_csv(file_path)
-        concat_df = pd.concat([concat_df, new_df], axis=0, ignore_index=True)
     return concat_df, sub_ids
 
 def calc_diff_presented_percieved(df):
@@ -290,7 +375,18 @@ def get_delta_presented(df):
         delta_presented.append(delta)
     delta_presented.append(np.nan)
     return delta_presented
+
+def get_means_df(df):
     
+    # group by speaker distances and get mean of responses
+    grouped_df = df.groupby('speaker_distance').agg(
+        mean_response=('response', 'mean'),
+        std_response=('response', 'std')
+        ).reset_index()
+    
+    # rename columns
+    grouped_df.columns = ['speaker_distance', 'mean_response', 'std_response']
+    return grouped_df
 
 def get_linear_regression_values(df, x_values, y_values):
     
--- run_analysis.py
+++ run_analysis.py
@@ -4,6 +4,7 @@ import analysis
 # global variables
 sub_id = 'all' # 'all' for concatenated data points
 cond_id = 1
+block_ids = [2, 4, 6]
 split = False
 
 # predict sample size
@@ -13,13 +14,19 @@ split = False
 # analysis.create_diagnostic_plots(sub_id=sub_id, cond_id=cond_id, block_id=4)
 
 # plot presented vs percieved distance of block 1, 4 and 6
-df = analysis.plot_presented_vs_percieved_distance(sub_id=sub_id, cond_id=cond_id, split=split)
+# df = analysis.plot_presented_vs_percieved_distance(sub_id=sub_id, cond_id=cond_id, block_ids=block_ids, split=split)
 
 # plot signed error distribution at specific speaker distance
-# analysis.plot_signed_error_distribution_at_x(cond_id=cond_id, block_id=6, x=12)
+# analysis.plot_signed_error_distribution_at_x(cond_id=cond_id, block_id=2, x=12)
 
 # seperate slope model
 # analysis.seperate_slope_model() # more work has to be done
 
 # plot differences
 # df = analysis.plot_differences(sub_id=sub_id, cond_id=cond_id, block_id=1) # weiß ich nicht...
+
+# calculate mean
+# means_df = analysis.plot_means(sub_id=sub_id, cond_id=cond_id, block_ids=block_ids)
+
+# plot data
+analysis.plot_data(cond_id=cond_id, block_ids=block_ids)
