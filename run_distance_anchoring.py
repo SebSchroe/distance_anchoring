90a1ee038c4f9550d512a88069f9d623d33e4709
SebSchroe <seb.schroe1@gmail.com>
2024-10-23 18:43:54 +0200

    Code optimization

---
+++ run_distance_anchoring.py
@@ -0,0 +1,25 @@
+# import necessary modules
+import freefield
+import distance_anchoring
+
+# set global variables
+sub_id = '01' # choose 'test_run' for functionality check (only 1 trial per speaker)
+cond_id = 1
+block_id = 6
+
+"""
+cond_id:    1 -> block 1, 2, 3, 4, 5 with task_id = 2
+                 block 6 with task_id = 1
+            2 -> block 1, 2, 3, 4, 5 with task_id = 3
+                 block 6 with task_id = 1
+
+task_id:    1 -> nearest_speaker_id = 0, farthest_speaker_id = 10
+            2 -> nearest_speaker_id = 1, farthest_speaker_id = 5
+            3 -> nearest_speaker_id = 5, farthest_speaker_id = 9
+"""
+# initialize setup
+distance_anchoring.initialize_setup()
+
+# start experiment block by block and flush all buffers after each block
+distance_anchoring.start_block(sub_id=sub_id, cond_id=cond_id, block_id=block_id)
+freefield.flush_buffers(processor='RX81')
