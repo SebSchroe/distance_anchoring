52c5de93991cd679becb65f1d559912692c4d57a
SebSchroe <seb.schroe1@gmail.com>
2024-11-15 13:30:25 +0100

    sub-05, sub-15 experiment

--- run_distance_anchoring.py
+++ run_distance_anchoring.py
@@ -4,8 +4,8 @@ import distance_anchoring
 import LedControl
 
 # set global variables
-sub_id = '26'  # choose 'test_run' for functionality check (training with task_id 1 and n_reps 11)
-cond_id = 2
+sub_id = '21'  # choose 'test_run' for functionality check (training with task_id 1 and n_reps 11)
+cond_id = 1
 block_id = 6
 
 """
