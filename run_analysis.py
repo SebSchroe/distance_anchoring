c6f3720651848e44a4f8817a2d81eba35e0b6b1c
SebSchroe <seb.schroe1@gmail.com>
2024-11-21 17:47:02 +0100

    more diagnostics

--- run_analysis.py
+++ run_analysis.py
@@ -12,6 +12,10 @@ sub_ids = ['01', '03', '05', '09', '13', '15', '19', '21', '25',
 cond_ids = [1, 2]
 block_ids = [1, 2, 4, 6]
 
+# get number of sub_ids per condition
+n_cond_1 = sum(int(sub_id) % 2 != 0 for sub_id in sub_ids)
+n_cond_2 = sum(int(sub_id) % 2 == 0 for sub_id in sub_ids)
+
 # load and transform data
 df = analysis.get_concat_df(sub_ids=sub_ids)
 df['speaker_distance'] = df['speaker_id'].apply(lambda x: analysis.get_speaker_distance(x, speaker_dict))
@@ -70,30 +74,11 @@ analysis.plot_boxplot(df=means_df, x='speaker_distance', y='mean_led_distance', 
 # analysis.plot_data(df=means_df, x='speaker_distance', y='mean_led_distance',
 #                    col='block_id', row='cond_id', hue='sub_id', kind='regplot')
 
-# %% calculate raw experiment duration
-# mean_response_time_df = df.groupby(['sub_id', 'block_id', 'cond_id', 'event_id'], as_index=False).agg(mean_response_time=('response_time', 'mean'))
-# analysis.plot_data(df=mean_response_time_df, x='event_id', y='mean_response_time',
-#                    col='block_id', row='cond_id', hue='sub_id', kind='scatterplot', baseline=False)
-
-# # exclude all response times with event_id = 1
-# filtered_mean_response_time_df = mean_response_time_df.query('event_id != 1')
-
-# # calculate mean response time
-# mean_response_time = filtered_mean_response_time_df['mean_response_time'].mean()
-
-# # %% calculate experiment duration
-# n_reps_list = [8, 9, 10, 11, 12, 13, 14, 15]
-# exp_dur_list = []
-
-# for n_reps in n_reps_list:
-#     exp_dur = analysis.calc_experiment_duration(n_reps=n_reps, mean_response_time=mean_response_time)
-#     exp_dur_list.append(exp_dur)
-    
-# sns.scatterplot(x=n_reps_list, y=exp_dur_list)
-    
 # %% predict sample size
-analysis.predict_sample_size(effect_size=1.501, alpha=0.05, power=0.8, alternative='two-sided')
+# analysis.predict_sample_size(effect_size=1.501, alpha=0.05, power=0.8, alternative='two-sided')
 
 # %% diagnostic plots
-# analysis.create_diagnostic_plots(df=df, x='speaker_distance', y='led_distance')
+# TODO: filter df for specific block and specific cond
+diagnostic_df = means_df[(means_df['cond_id'] == 2) & (means_df['block_id'] == 1)]
+analysis.create_diagnostic_plots(df=diagnostic_df, x='speaker_distance', y='mean_led_distance')
 
